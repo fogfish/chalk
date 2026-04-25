dafca39816fb5958c597c5760978e5b1718b060b
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2026-04-25 22:58:47 +0300

    (fix) inject reporter into context

---
+++ boot.go
@@ -0,0 +1,93 @@
+//
+// Copyright (C) 2026 Dmitry Kolesnikov
+//
+// This file may be modified and distributed under the terms
+// of the MIT license.  See the LICENSE file for details.
+// https://github.com/fogfish/chalk
+//
+
+package chalk
+
+import (
+	"context"
+	"flag"
+	"os"
+	"time"
+
+	"github.com/charmbracelet/x/term"
+	"github.com/fogfish/opts"
+	"github.com/fogfish/stream/spool"
+)
+
+const ctxkey = "github.com/fogfish/chalk"
+
+var stdout *Reporter
+var noTTY bool
+var noColor bool
+
+// NoTTY forces log-mode output (structured slog records, no colour, no spinner)
+// even when stderr is an interactive terminal. Must be called before Start.
+func NoTTY() { noTTY = true }
+
+// NoColor disables color output in TTY mode, using plain black & white styles.
+// Must be called before Start.
+func NoColor() { noColor = true }
+
+// Init creates a Reporter that writes to stderr, automatically selecting
+// the ttyPrinter or logPrinter strategy based on whether stderr is a terminal.
+// If NoTTY has been called, logPrinter is always used regardless of the terminal.
+func Init() *Reporter {
+	flag.Parse()
+
+	if *flagNoTTY {
+		noTTY = true
+	}
+
+	if *flagNoColor {
+		noColor = true
+	}
+
+	r := &Reporter{}
+	start := time.Now()
+	if !noTTY && term.IsTerminal(os.Stderr.Fd()) {
+		if noColor {
+			bwStyles()
+		}
+		r.p = newTTYPrinter(os.Stderr, start, &r.mu)
+	} else {
+		r.p = &logPrinter{}
+	}
+
+	stdout = r
+	return r
+}
+
+// Start wires up the default Reporter, parses flags, and runs the provided
+// Spooler. It is the main entry-point for CLI tools built on this library.
+func Start(f spool.Spooler) {
+	Init()
+
+	src, wlk, err := source()
+	if err != nil {
+		Panic(err)
+	}
+
+	dst, err := target()
+	if err != nil {
+		Panic(err)
+	}
+
+	var opts []opts.Option[spool.Spool]
+	if extOverride != nil && *extOverride != "" {
+		opts = append(opts, spool.WithFileExt(*extOverride))
+	}
+	ingress := spool.New(src, dst, opts...)
+
+	ctx := context.WithValue(context.Background(), ctxkey, stdout)
+
+	if err = ingress.ForEach(ctx, wlk, f); err != nil {
+		Panic(err)
+	}
+
+	stdout.Quit()
+}
